0eb3dd89397eb7fddafa1b5d7377ec7e4e44dbe6
mcdonghyunkim <deepmining3825@gmail.com>
2023-01-15 18:04:57 +0900

    Initial commit

---
+++ src/main/java/com/dkadtech/hawaiikai/HawaiiKaiApplication.java
@@ -0,0 +1,13 @@
+package com.dkadtech.hawaiikai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HawaiiKaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HawaiiKaiApplication.class, args);
+    }
+
+}
